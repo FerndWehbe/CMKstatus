5e9c29808c81b2a46b38d5d3c2316f3a557d1717
FerndWehbe <51804490+FerndWehbe@users.noreply.github.com>
2022-01-22 21:38:27 -0300

    Create statuscmk.py

---
+++ statuscmk.py
@@ -0,0 +1,28 @@
+import subprocess
+import logging
+
+pros_status = subprocess.Popen("omd status check_mk", shell=True, stdout=subprocess.PIPE)
+logging.basicConfig(filename="teste.log", format="%(asctime)s %(levelname)s %(message)s", level=logging.DEBUG)
+
+logger = logging.getLogger("Service")
+
+output = pros_status.stdout.read().decode()
+
+dict_service = {}
+
+for out in output.split("\n"):
+    if "------" in out:
+        break
+    splited = out.split(":")
+    dict_service[splited[0].strip()] = splited[1].strip()
+
+for service, status in dict_service.items():
+    if status == "stopped":
+        logger.critical(f"O Serviço: {service} parou!")
+        pros_restart = subprocess.Popen(f"omd restart check_mk {service}", shell=True, stdout=subprocess.PIPE)
+        restarted = pros_restart.stdout.read().decode()
+        if f"Starting {service}...OK" in restarted:
+            logger.info(f"O serviço {service} foi restartado com sucesso.")
+        else:
+            logger.critical(f"O restart do serviço {service} falhor")
+            logger.critical(restarted)
