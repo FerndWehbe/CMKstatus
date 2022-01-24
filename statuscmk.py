49f10e121dbd19b78c2c403a3f1c97858625cfab
FerndWehbe <51804490+FerndWehbe@users.noreply.github.com>
2022-01-24 10:43:25 -0300

    Update statuscmk.py

--- statuscmk.py
+++ statuscmk.py
@@ -1,8 +1,22 @@
+from time import sleep
 import subprocess
 import logging
+import os
+import re
 
-pros_status = subprocess.Popen("omd status check_mk", shell=True, stdout=subprocess.PIPE)
-logging.basicConfig(filename="teste.log", format="%(asctime)s %(levelname)s %(message)s", level=logging.DEBUG)
+
+SITE_NAME = "check_mk"
+PATH_LOG = ""
+FILE_NAME = "cmkstatus.log"
+
+pros_status = subprocess.Popen(
+    f"omd status {SITE_NAME}", shell=True, stdout=subprocess.PIPE
+)
+logging.basicConfig(
+    filename=os.path.join(PATH_LOG, FILE_NAME),
+    format="%(asctime)s %(levelname)s %(message)s",
+    level=logging.INFO,
+)
 
 logger = logging.getLogger("Service")
 
@@ -19,9 +33,13 @@ for out in output.split("\n"):
 for service, status in dict_service.items():
     if status == "stopped":
         logger.critical(f"O Serviço: {service} parou!")
-        pros_restart = subprocess.Popen(f"omd restart check_mk {service}", shell=True, stdout=subprocess.PIPE)
+        pros_restart = subprocess.Popen(
+            f"omd restart check_mk {service}",
+            shell=True,
+            stdout=subprocess.PIPE,
+        )
         restarted = pros_restart.stdout.read().decode()
-        if f"Starting {service}...OK" in restarted:
+        if re.search(f"Starting {service}(.*?)...OK", restarted):
             logger.info(f"O serviço {service} foi restartado com sucesso.")
         else:
             logger.critical(f"O restart do serviço {service} falhou")
